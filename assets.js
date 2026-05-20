0286afd7448eca007fbd37996a0ba07f02bfc21e
Gituser-PROgrammer <139343533+Gituser-PROgrammer@users.noreply.github.com>
2026-05-20 13:19:31 +0530

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,10 +1,10 @@
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/favicon.png",
-  "/favicon128.png",
-  "/favicon256.png",
-  "/favicon512.png",
-  "/script.js",
-  "/sw-register.js"
+  "./",
+  "./index.html",
+  "./favicon.png",
+  "./favicon128.png",
+  "./favicon256.png",
+  "./favicon512.png",
+  "./script.js",
+  "./sw-register.js"
 ];
