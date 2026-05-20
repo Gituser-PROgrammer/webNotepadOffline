e4a301622547dbde90c5400c776474dcacb473d3
Gituser-PROgrammer <139343533+Gituser-PROgrammer@users.noreply.github.com>
2026-05-20 12:44:52 +0530

    Create sw-register.js

---
+++ sw-register.js
@@ -0,0 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      await navigator.serviceWorker.register("/sw.js");
+
+      console.log("Service Worker Registered");
+    } catch (err) {
+      console.error("Service Worker Failed:", err);
+    }
+  });
+}
